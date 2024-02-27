d3b5b480ac2bea484b906b5a19b57f93a6676755
billifotr <billifotr@gmail.com>
2024-02-27 17:49:42 +0100

    fckn qt

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import sys
 from PyQt6.QtCore import Qt
 
 # from PyQt6.QtGui import QGuiApplication
+
 from PyQt6.QtGui import QPalette, QColor
-from PyQt6.QtWidgets import QMainWindow, QWidget, QApplication, QLayout
+from PyQt6.QtWidgets import QMainWindow, QWidget, QApplication, QHBoxLayout, QLayout, QPushButton
+
 # from PyQt6.QtQml import QQmlApplicationEngine
 # from PyQt6.QtQuick import QQuickWindow
 
@@ -11,7 +13,8 @@ class Color(QWidget):
   
   def __init__(self, color):
     super(Color, self).__init__()
-    self.autoFillBackground(True)
+
+    # self.autoFillBackground(True)
 
     palette = self.palette()
     palette.setColor(QPalette.ColorRole.Window, QColor(color))
@@ -23,17 +26,19 @@ class MainWindow(QMainWindow):
     super(MainWindow, self).__init__()
 
     self.setWindowTitle("App")
-
-    layout = QHBoxLayout()
-
-    layout.addWidget(Color('Red'))
-    layout.addWidget(Color('Green'))
-    layout.addWidget(Color('Blue'))
+    self.setMinimumSize(1024, 512)
 
     widget = QWidget()
+    layout = QHBoxLayout()
+
+    button1 = QPushButton("Add")
+    button2 = QPushButton("Take")
+
+    layout.addWidget(button1)
+    layout.addWidget(button2)
+    
     widget.setLayout(layout)
     self.setCentralWidget(widget)
-
 
 app = QApplication(sys.argv)
 
